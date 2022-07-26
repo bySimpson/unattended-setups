9816d0c41fc826d917781a381e0d3597d25c46c5
Simon Thamer <thamers@edu.htl-villach.at>
2022-07-26 20:25:37 +0200

    changed apt download from wget to curl

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt install zsh git wget -y
+sudo apt update && sudo apt install zsh git curl -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting && cd ~
 sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"risto\"/g" ~/.zshrc
