c15dba60a625ac1b7e7f58a512fe0afb80285972
Simon Thamer <thamers@edu.htl-villach.at>
2022-07-26 20:36:44 +0200

    fixed typo in setup-zsh.sh

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -4,5 +4,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting && cd ~
 sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"risto\"/g" ~/.zshrc
 sed -i "s/plugins=.*/plugins=(git sudo zsh-syntax-highlighting git dirhistory extract web-search yum git-extras docker vagrant)/g" ~/.zshrc
-sed -i '/unsetopt BEEP/d' ~/.zshrc && echo "unsetopt BEEP" >> ~.zshrc
+sed -i '/unsetopt BEEP/d' ~/.zshrc && echo "unsetopt BEEP" >> ~/.zshrc
 exec /bin/zsh
