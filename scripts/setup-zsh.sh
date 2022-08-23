04f257a9fe5056a86c52630980f21a5448ff3dfb
bySimpson <47668312+bySimpson@users.noreply.github.com>
2022-08-23 21:17:54 +0200

    Update setup-zsh.sh

--- scripts/setup-zsh.sh
+++ scripts/setup-zsh.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt install zsh git curl -y
+sudo apt update && sudo apt install zsh git curl fortune cowsay -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting && cd ~
 sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"risto\"/g" ~/.zshrc
-sed -i "s/plugins=.*/plugins=(git sudo zsh-syntax-highlighting git dirhistory extract web-search yum git-extras docker vagrant)/g" ~/.zshrc
+sed -i "s/plugins=.*/plugins=(git sudo zsh-syntax-highlighting git dirhistory extract web-search yum git-extras docker vagrant chucknorris)/g" ~/.zshrc
 sed -i '/unsetopt BEEP/d' ~/.zshrc && echo "unsetopt BEEP" >> ~/.zshrc
 exec /bin/zsh
